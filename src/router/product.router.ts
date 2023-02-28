dcd29ee176e686b5fd6b8b129eb53c74d60f7557
diegomated1 <diegocardwr@gmail.com>
2023-02-28 15:44:40 -0500

    add route for get favorites

--- src/router/product.router.ts
+++ src/router/product.router.ts
@@ -7,6 +7,7 @@ const productRouter = Router();
 
 productRouter.route('/products').post(handle_auth, Product.insert);
 productRouter.route('/products').get(handle_auth, Product.getAll);
+productRouter.route('/products/favorite').get(handle_auth, Product.getFavorites);
 productRouter.route('/products/:id_product').get(handle_auth, Product.getOneById);
 productRouter.route('/products/favorite/:id_product').post(handle_auth, Product.addFavorite);
 productRouter.route('/products/favorite/:id_product/').delete(handle_auth, Product.deleteFavorite);
