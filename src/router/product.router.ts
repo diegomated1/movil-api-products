00acb052d22e31f34f9a840abedf38406e541908
diegomated1 <diegocardwr@gmail.com>
2023-02-28 15:33:32 -0500

    add routes for add and delete products from favorites

--- src/router/product.router.ts
+++ src/router/product.router.ts
@@ -8,5 +8,7 @@ const productRouter = Router();
 productRouter.route('/products').post(handle_auth, Product.insert);
 productRouter.route('/products').get(handle_auth, Product.getAll);
 productRouter.route('/products/:id_product').get(handle_auth, Product.getOneById);
+productRouter.route('/products/favorite/:id_product').post(handle_auth, Product.addFavorite);
+productRouter.route('/products/favorite/:id_product/').delete(handle_auth, Product.deleteFavorite);
 
 export default productRouter;
