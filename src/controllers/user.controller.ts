d288441ed7c05821f22b89a8f9e51719d257ad85
diegomated1 <diegocardwr@gmail.com>
2023-02-28 14:33:02 -0500

    delete password attr where from auth controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,6 +8,9 @@ import IUser from 'interfaces/IUser.js';
 export const login = async (req: Request, res: Response)=>{
     try{
         const {email, password} = req.body;
+        if(email==undefined || password==undefined){
+            return res.status(400).json({error: 2, message: 'Invalid credentials'});
+        }
         const _user = await User.getByEmail(email);
         if(_user){
             const math = await bc.compare(password, _user.password!);
@@ -15,10 +18,10 @@ export const login = async (req: Request, res: Response)=>{
                 const token = jwt.sign({id_user: _user.id_user}, process.env.JWT_SECRET!);
                 res.status(200).json({error: 0, message: 'Logged', userToken: token});
             }else{
-                res.status(401).json({error: 2, message: 'Invalid credentials'});
+                res.status(401).json({error: 4, message: 'Invalid credentials'});
             }
         }else{
-            res.status(404).json({error: 1, message: 'User not found'});
+            res.status(404).json({error: 3, message: 'User not found'});
         }
     }catch(error){
         console.log(error);
@@ -28,14 +31,18 @@ export const login = async (req: Request, res: Response)=>{
 
 export const register = async (req: Request, res: Response)=>{
     try{
-        const user = req.body.user as IUser;
-        const _user = await User.getByEmail(user.email);
+        const {email, username, password} = req.body;
+        if(email == undefined || username == undefined || password == undefined){
+            return res.status(400).json({error: 2, message: 'Invalid credentials'});
+        }
+        const _user = await User.getByEmail(email);
         if(_user){
+            console.log(_user);
             return res.status(400).json({error: 1, message: 'Email already taken'});
         }
         const id_user = ui.process();
-        const password = await bc.hash(user.password!, 10);
-        await User.insert({...user, id_user, password});
+        const _password = await bc.hash(password!, 10);
+        await User.insert({email, username, id_user, password: _password});
         const token = jwt.sign({id_user}, process.env.JWT_SECRET!);
         res.status(200).json({error: 0, message: 'Registered', userToken: token});
     }catch(error){
@@ -47,6 +54,7 @@ export const register = async (req: Request, res: Response)=>{
 export const auth = async (req: Request, res: Response)=>{
     try{
         const user = await User.getById(res.locals.id_user);
+        delete user.password;
         res.status(200).json({succes: true, error: 0, user});
     }catch(error){
         res.status(400).json({succes: false, error: 1, message: (error as Error).message});
