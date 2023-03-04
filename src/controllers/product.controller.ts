c6bc06015e01961eb123ea70456f39114dc2c9cf
diegomated1 <diegocardwr@gmail.com>
2023-03-04 12:26:45 -0500

    saved image in insert controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,6 +3,7 @@ import * as User from '../services/user.services.js';
 import {Request, Response} from 'express';
 import IProduct from 'interfaces/IProducts.js';
 import ui from 'uniqid';
+import path from 'path';
 
 export const getOneById = async (req: Request, res: Response)=>{
     try{
@@ -33,9 +34,18 @@ export const getAll = async (req: Request, res: Response)=>{
 export const insert = async (req: Request, res: Response)=>{
     try{
         const product = req.body as IProduct;
-        const id_product = ui.process();
-        await Product.insert({...product, id_product});
-        res.status(200).json({error: 0, message: 'Inserted'});
+        if(req.file){
+            var id_product = path.parse(req.file.filename).name;
+        }else{
+            var id_product = ui.process();
+        }
+        const _user = await User.getById(res.locals.id_user);
+        if(_user){
+            await Product.insert({...product, id_product, id_seller: _user.id_user, seller_name: _user.username});
+            res.status(200).json({error: 0, message: 'Inserted'});
+        }else{
+            res.status(404).json({error: 0, message: 'User not found'});
+        }
     }catch(error){
         console.log(error);
         res.status(500).json({error: 1, message: 'Internal server error.'});
